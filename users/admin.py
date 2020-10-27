6454c041e4d3d294a132c115b345c44d71ac2c83
jangtang2 <58350257+jangtang2@users.noreply.github.com>
2020-10-27 19:54:01 +0900

    edit user model 3.5

--- users/admin.py
+++ users/admin.py
@@ -1,13 +1,23 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from . import models
 
 # Register your models here.
 
 
 @admin.register(models.User)
-class CustomUserAdmin(admin.ModelAdmin):
+class CustomUserAdmin(UserAdmin):
 
     """Custom User Admin"""
 
-    list_display = ("username", "email", "date_joined", "admin")
-    list_filter = ("date_joined", "admin")
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Custom Profile",
+            {
+                "fields": (
+                    "photo",
+                    "admin",
+                )
+            },
+        ),
+    )
