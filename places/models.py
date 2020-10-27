fa379704e23edfb7d737c5d5326d9db7f704f111
jangtang2 <58350257+jangtang2@users.noreply.github.com>
2020-10-27 20:51:41 +0900

    create place db 4.1

--- places/models.py
+++ places/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django_countries.fields import CountryField
 from core import models as core_models
 
 # Create your models here.
@@ -8,4 +9,12 @@ class Place(core_models.TimeStampedModel):
 
     """ Place Model Def"""
 
-    pass
+    name = models.CharField(max_length=140)
+    description = models.TextField()
+    country = CountryField()
+    city = models.CharField(max_length=80)
+    address = models.CharField(max_length=140)
+    toilets = models.IntegerField()
+    showerbooths = models.IntegerField()
+    operdays = models.DateField()
+    booklinks = models.URLField()
