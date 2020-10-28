5cd209062ed30fdd21c9d5448a8aa9544e508f91
jangtang2 <58350257+jangtang2@users.noreply.github.com>
2020-10-28 19:16:40 +0900

    setting up place info 6.1

--- places/admin.py
+++ places/admin.py
@@ -23,13 +23,51 @@ class ItemAdmin(admin.ModelAdmin):
 @admin.register(models.Place)
 class PlaceAdmin(admin.ModelAdmin):
 
+    fieldsets = (
+        (
+            "Basic Info",
+            {
+                "fields": ("name", "description", "city", "address"),
+            },
+        ),
+        (
+            "Spaces",
+            {
+                "fields": (
+                    "toilets",
+                    "showerbooths",
+                )
+            },
+        ),
+        (
+            "More About The Space",
+            {
+                "fields": (
+                    "amenities",
+                    "facilities",
+                    "etcs",
+                )
+            },
+        ),
+        (
+            "For Reservation",
+            {"fields": ("booklinks",)},
+        ),
+    )
+
     list_display = (
         "name",
         "city",
         "booklinks",
     )
+
     list_filter = ("city",)
     search_fields = ("=city", "^name")
+    filter_horizontal = (
+        "amenities",
+        "facilities",
+        "etcs",
+    )
 
 
 @admin.register(models.Photo)
