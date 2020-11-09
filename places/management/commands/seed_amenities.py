1e9d0826dbe623294f125977228ecacf64ae9f75
jangtang2 <58350257+jangtang2@users.noreply.github.com>
2020-11-10 02:15:10 +0900

    seed ameneties 9.1

---
+++ places/management/commands/seed_amenities.py
@@ -0,0 +1,23 @@
+from django.core.management.base import BaseCommand
+from places.models import Amenity
+
+
+class Command(BaseCommand):
+    help = "this command creates amenities"
+
+    def handle(self, *args, **options):
+        amenities = [
+            "전기",
+            "와이파이",
+            "장작판매",
+            "온수",
+            "산책로",
+            "물놀이장",
+            "운동시설",
+            "마트, 편의점",
+            "놀이터" "운동장",
+            "트램폴린",
+        ]
+        for a in amenities:
+            Amenity.objects.create(name=a)
+        self.stdout.write(self.style.SUCCESS("Amenities created"))
