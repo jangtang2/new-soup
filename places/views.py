1195497fae0fe4cc48ad99add441301301215098
jangtang2 <58350257+jangtang2@users.noreply.github.com>
2020-11-17 07:34:44 +0900

    create navigation 11.5

--- places/views.py
+++ places/views.py
@@ -1,10 +1,20 @@
+from math import ceil
 from django.shortcuts import render
+from django.core.paginator import Paginator
 from . import models
 
 
 def all_places(request):
-    all_places = models.Place.objects.all()
-    return render(request, "places/home.html", context={"places": all_places})
+    page = request.GET.get("page", 1)
+    place_list = models.Place.objects.all()
+    paginator = Paginator(place_list, 10, orphans=5)
+    places = paginator.get_page(int(page))
+
+    return render(
+        request,
+        "places/home.html",
+        {"places": places},
+    )
 
 
 # Create your views here.
