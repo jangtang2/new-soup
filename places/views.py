9ecad5461634431116b2c6c270acbf5993c773cc
jangtang2 <58350257+jangtang2@users.noreply.github.com>
2020-11-17 06:02:39 +0900

    test templates 10.4

--- places/views.py
+++ places/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from . import models
 
 
 def all_places(request):
-    return render(request, "all_places")
+    all_places = models.Place.objects.all()
+    return render(request, "places/home.html", context={"places": all_places})
 
 
 # Create your views here.
