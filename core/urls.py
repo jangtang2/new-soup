1a7035d3936f488dafec42f96475a0ba79ae98bf
jangtang2 <58350257+jangtang2@users.noreply.github.com>
2020-11-17 13:55:31 +0900

    view set template 11.7

--- core/urls.py
+++ core/urls.py
@@ -3,4 +3,4 @@ from places import views as place_views
 
 app_name = "core"
 
-urlpatterns = [path("", place_views.all_places, name="home")]
+urlpatterns = [path("", place_views.HomeView.as_view(), name="home")]
--- places/views.py
+++ places/views.py
@@ -1,20 +1,12 @@
-from math import ceil
-from django.shortcuts import render
-from django.core.paginator import Paginator
+from django.views.generic import ListView
 from . import models
 
 
-def all_places(request):
-    page = request.GET.get("page", 1)
-    place_list = models.Place.objects.all()
-    paginator = Paginator(place_list, 10, orphans=5)
-    places = paginator.get_page(int(page))
-
-    return render(
-        request,
-        "places/home.html",
-        {"places": places},
-    )
+class HomeView(ListView):
+    model = models.Place
+    paginate_by = 10
+    paginate_orphans = 5
+    ordering = "created"
 
 
 # Create your views here.
